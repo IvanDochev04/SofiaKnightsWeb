b9523286d1abad2be0dfce4dbf0d703aa3282f29
Ivan Dochev <ivan.dochev@accedia.com>
2021-08-27 05:44:02 +0300

    Arhitecture and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { FixturesModule } from './fixtures/fixtures.module';
+import { NewsModule } from './news/news.module';
+import { NotFoundModule } from './not-found/not-found.module';
+import { TeamModule } from './team/team.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,11 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    TeamModule,
+    NewsModule,
+    NotFoundModule,
+    FixturesModule,
     AppRoutingModule,
     CoreModule
   ],
